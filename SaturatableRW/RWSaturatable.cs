5c73c971da8e677b8baddfab52cf0d51f6242e8c
Crzyrndm <joshcrawfy@gmail.com>
2015-05-03 10:01:33 +1200

    .version

--- SaturatableRW/RWSaturatable.cs
+++ SaturatableRW/RWSaturatable.cs
@@ -128,7 +128,6 @@ namespace SaturatableRW
             }
             catch (Exception ex)
             {
-                Debug.Log(ex.InnerException);
                 Debug.Log(ex.StackTrace);
             }
         }
